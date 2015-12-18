7626cded93c61d0e7fc31104d14781a7d66bd582
Rudie Dirkx <github@hotblocks.nl>
2015-12-18 23:26:17 +0100

    +autoload

--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-spl_autoload_register(function($class) {
-	if ( file_exists($file = __DIR__ . '/' . str_replace('\\', '/', str_replace('rdx\\wikiparser\\', 'src\\', $class)) . '.php') ) {
-		include $file;
-	}
-});
+require __DIR__ . '/lib/wikiparser/autoload.php';
 
 header('Content-type: text/html; charset=utf-8');
